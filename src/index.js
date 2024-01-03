27650d856f9f126169cd94b23ed085ae390a2ae1
Kaio Silveira <silveira.kaio@icloud.com>
2024-01-03 20:44:25 +0000

    setup initial examples + test + CI

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-export function toBeRefactored() {
-  return 'Hello, world!';
+export function renderPerson(outStream, person) {
+  outStream.write(`<p>${person.name}</p>\n`);
+  renderPhoto(outStream, person.photo);
+  emitPhotoData(outStream, person.photo);
+}
+
+export function listeRecentPhotos(outStream, photos) {
+  photos
+    .filter(p => p.date > recentDateCutoff())
+    .forEach(p => {
+      outStream.write(`<div>\n`);
+      emitPhotoData(outStream, p);
+      outStream.write(`</div>\n`);
+    });
+}
+
+function emitPhotoData(outStream, photo) {
+  outStream.write(`<p>title: ${photo.title}</p>\n`);
+  outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
+  outStream.write(`<p>location: ${photo.location}</p>\n`);
+}
+
+function recentDateCutoff() {
+  return new Date(2019, 0, 1);
+}
+
+function renderPhoto(outStream, _photo) {
+  outStream.write(`<p>🌃</p>\n`);
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,39 @@
-import { toBeRefactored } from './index';
+import { jest } from '@jest/globals';
+import { listeRecentPhotos, renderPerson } from '.';
 
-describe('functionToBeRefactored', () => {
-  it('should work', () => {
-    expect(toBeRefactored()).toEqual('Hello, world!');
+describe('renderPerson', () => {
+  it('should render the correct markup', () => {
+    const photo = { title: 'My Photo', location: 'Paris', date: new Date(2020, 0, 1) };
+    const person = { name: 'John Doe', photo };
+    const outStream = { write: jest.fn() };
+
+    renderPerson(outStream, person);
+
+    expect(outStream.write).toHaveBeenCalledTimes(5);
+    expect(outStream.write).toHaveBeenCalledWith(`<p>John Doe</p>\n`);
+    expect(outStream.write).toHaveBeenCalledWith(`<p>🌃</p>\n`);
+    expect(outStream.write).toHaveBeenCalledWith(`<p>title: My Photo</p>\n`);
+    expect(outStream.write).toHaveBeenCalledWith(`<p>date: Wed Jan 01 2020</p>\n`);
+    expect(outStream.write).toHaveBeenCalledWith(`<p>location: Paris</p>\n`);
+  });
+});
+
+describe('listRecentPhotos', () => {
+  it('should render the correct markup', () => {
+    const outStream = { write: jest.fn() };
+    const photos = [
+      { title: 'Photo 1', location: 'Paris', date: new Date(2020, 0, 1) },
+      { title: 'Photo 2', location: 'Paris', date: new Date(2019, 0, 1) },
+      { title: 'Photo 3', location: 'Paris', date: new Date(2018, 0, 1) },
+    ];
+
+    listeRecentPhotos(outStream, photos);
+
+    expect(outStream.write).toHaveBeenCalledTimes(5);
+    expect(outStream.write).toHaveBeenCalledWith(`<div>\n`);
+    expect(outStream.write).toHaveBeenCalledWith(`<p>title: Photo 1</p>\n`);
+    expect(outStream.write).toHaveBeenCalledWith(`<p>date: Wed Jan 01 2020</p>\n`);
+    expect(outStream.write).toHaveBeenCalledWith(`<p>location: Paris</p>\n`);
+    expect(outStream.write).toHaveBeenCalledWith(`</div>\n`);
   });
 });
